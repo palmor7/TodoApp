4c0fb3bfc7cd481f767f62672841aa3ed0146d22
Kostas Premtsis <premtsis_7@hotmail.com>
2018-10-04 04:50:42 +0300

    added a playground folder in .gitignore and in mongoose.js added an object in connection to the database to avoid some warnings

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/todoApp');
+mongoose.connect('mongodb://localhost:27017/todoApp', {useNewUrlParser: true});
 
 module.exports = {mongoose};
 
