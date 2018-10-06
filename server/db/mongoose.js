7d3cccbe9ccfc15e34eff12828ea087183b44dbd
Kostas Premtsis <premtsis_7@hotmail.com>
2018-10-06 21:57:04 +0300

    some changes in mongodb connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://palmor7:olumpiakoss7@ds123513.mlab.com:23513/todoapp', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/todoApp' || 'mongodb://palmor7:olumpiakoss7@ds123513.mlab.com:23513/todoapp', {useNewUrlParser: true});
 
 module.exports = {mongoose};
 
 
+//'mongodb://palmor7:olumpiakoss7@ds123513.mlab.com:23513/todoapp' || 
