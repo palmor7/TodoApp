0c0b12322f6f2f6868f646df784e3d90cab6c4c2
Kostas Premtsis <premtsis_7@hotmail.com>
2018-10-06 22:30:48 +0300

    changed the inputs in the mongodb.connent()

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://palmor7:olumpiakoss7@ds123513.mlab.com:23513/todoapp', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todoApp', {useNewUrlParser: true});
 
 module.exports = {mongoose};
 
