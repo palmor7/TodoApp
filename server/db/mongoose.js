4effa53defa97de6b8ed5e412fc9c20db68e6516
Kostas Premtsis <premtsis_7@hotmail.com>
2018-10-06 22:54:28 +0300

    change in db connection after setting the mongodb_lab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todoApp', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGOLAB_URL || 'mongodb://localhost:27017/todoApp');
 
 module.exports = {mongoose};
 
 
 //'mongodb://palmor7:olumpiakoss7@ds123513.mlab.com:23513/todoapp' || 
 //('mongodb://localhost:27017/todoApp' || 
+//, {useNewUrlParser: true}
