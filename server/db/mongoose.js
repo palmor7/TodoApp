8575a83f9e062877af4d9fbd50d2c4ef3c6f3286
Kostas Premtsis <premtsis_7@hotmail.com>
2018-10-06 22:07:45 +0300

    trying to fix it

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(('mongodb://localhost:27017/todoApp' || 'mongodb://palmor7:olumpiakoss7@ds123513.mlab.com:23513/todoapp'), {useNewUrlParser: true});
+mongoose.connect('mongodb://palmor7:olumpiakoss7@ds123513.mlab.com:23513/todoapp', {useNewUrlParser: true});
 
 module.exports = {mongoose};
 
 
 //'mongodb://palmor7:olumpiakoss7@ds123513.mlab.com:23513/todoapp' || 
+//('mongodb://localhost:27017/todoApp' || 
